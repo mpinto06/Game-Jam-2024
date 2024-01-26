614ca6807ea559288c489ff0de3e4d1de0c3c805
Miguel Pinto <miguelep0106@gmail.com>
2024-01-26 16:25:28 -0400

    animaciion

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
+	if (velocity.x > 1) || (velocity.x < -1):
+		pass
+	else:
+		animated_sprite_2d.animation = "Idle"
 	# Add the gravity.
 	if not is_on_floor():
+		animated_sprite_2d.animation = "Jump"
 		velocity.y += gravity * delta
 
 	# Handle jump.
@@ -24,5 +29,9 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if velocity.x < 0:
+		animated_sprite_2d.flip_h = true
+	elif velocity.x > 0:
+		animated_sprite_2d.flip_h = false
 	move_and_slide()
