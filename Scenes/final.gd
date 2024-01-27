292b9347b07e2e0788e16414223d378de8b03fd9
Brillem <juandabrillemr@gmail.com>
2024-01-27 00:47:47 -0400

    final de nivel

---
+++ Scenes/final.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var target : PackedScene
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var timer = $Timer
+
+func _on_body_entered(body):
+	if body.name == "CharacterBody2D":
+		get_tree().paused = true
+		animated_sprite_2d.play("grabbed")
+		timer.start()
+		
+		
+
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_packed(target)
+
