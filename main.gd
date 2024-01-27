7e4712b6ab37dae122a3f548bf5a27a97abf7b95
Poletron <oscar.jt.fr@gmail.com>
2024-01-26 22:08:09 -0400

    Main menu troll

--- main.gd
+++ main.gd
@@ -7,15 +7,18 @@ func _on_play_pressed():
 	
 
 func _on_play_mouse_entered():
-	self.set_position(Vector2(-200,-200))
+	pass
+	
 
-func _on_play_mouse_exited():
-	self.set_position(Vector2(0,0))
+
 
 
 func _on_quit_pressed():
 	get_tree().change_scene_to_file("res://Scenes/nivel_1.tscn") # Replace with function body.
 
 
+func _on_area_2d_mouse_entered():
+	$Play.set_position(Vector2(-200,-200))
 
-	
+func _on_area_2d_mouse_exited():
+	$Play.set_position(Vector2(450, 180))
