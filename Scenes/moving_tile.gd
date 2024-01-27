72a22b42eac90e22489b128bba1ecc8489b55603
Miguel Pinto <miguelep0106@gmail.com>
2024-01-27 11:37:56 -0400

    aaaa

--- Scenes/moving_tile.gd
+++ Scenes/moving_tile.gd
@@ -3,12 +3,12 @@ extends AnimatableBody2D
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
 
 
-
+const SPEED = 450
 
 func _on_area_2d_body_entered(body):
 	if body.name == "MainCharacter":
-		pass
+		move_tile()
 
 func move_tile():
-	print(self.position)
+	print(velocity)
 
