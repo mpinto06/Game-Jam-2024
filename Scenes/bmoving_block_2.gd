fc5d0af9b263fffc29035908c008fca85701d794
Brillem <juandabrillemr@gmail.com>
2024-01-28 13:05:25 -0400

    nivel 4

--- Scenes/bmoving_block_2.gd
+++ Scenes/bmoving_block_2.gd
@@ -10,7 +10,7 @@ var run_away = false
 func _physics_process(delta):
 	if run_away and position.y > 150:
 		var direction = (player.position - self.position).normalized()
-		velocity.y = -SPEED * 15
+		velocity.y = -SPEED * 10
 		move_and_slide()
 	else:
 		run_away = false
