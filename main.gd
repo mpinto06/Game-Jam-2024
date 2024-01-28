b937d2e5abb7a3882870028cd375e928351f24a2
Poletron <oscar.jt.fr@gmail.com>
2024-01-27 21:53:06 -0400

    Boton Troll

--- main.gd
+++ main.gd
@@ -7,13 +7,18 @@ func _on_play_pressed():
 
 
 func _on_quit_pressed():
-	get_tree().change_scene_to_file("res://Scenes/nivel_1.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/nivel_1.tscn")
+	 # Replace with function body.
 
 
 func _on_area_2d_mouse_entered():
 	#$Play.set_physics_process()
 	#$Play.set_position(Vector2(-200,-200))
-	pass
+	var tween = get_tree().create_tween()
+	tween.tween_property($Play, "position", Vector2(-200,-100), 1)
+
+
 
 func _on_area_2d_mouse_exited():
-	$Play.set_position(Vector2(465, 180))
+	var tween = get_tree().create_tween()
+	tween.tween_property($Play, "position", Vector2(465,180), 0.8)
